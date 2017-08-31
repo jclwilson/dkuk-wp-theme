980d9e0f06133b11e57306d9d7209810f975a477
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-08-31 11:30:21 +0100

    Adds custom logo to functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,2 +1,21 @@
 // functions.php
 // Fully documented by @jclwilson
+
+function dkuk_setup() {
+
+	// Adds custom logo to Theme
+	// from the WordPress Developer Reference
+	// https://developer.wordpress.org/reference/functions/add_theme_support/#custom-logo
+
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100, // adjust this
+		'width'       => 400, // adjust this
+		'flex-height' => true,
+		'flex-width'  => true,
+		'header-text' => array( 'site-title', 'site-description' ), // check whether these are variables or set strings
+	) );
+
+	// Ends custom logo
+
+}
+add_action( 'after_setup_theme', 'dkuk_setup' );
