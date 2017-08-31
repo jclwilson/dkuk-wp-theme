c3823e820e60c22ba4b5ce2d0395d0d1706b8cfa
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-08-31 12:59:11 +0100

    Adds necessary HEAD elements & php tags

--- src/index.php
+++ src/index.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<!-- Set lang attribute according to blog default -->
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
+	<!-- Sets Charset according to blog default -->
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<!-- Sets title according to page type
+		Written by @jclwilson -->
+	<title>
+            <?php if (function_exists('is_tag') && is_tag()) {
+                echo 'Tag Archive for &quot;'.$tag.'&quot; - ';
+            } elseif (is_archive()) {
+                wp_title(''); echo ' Archive - ';
+            } elseif (is_search()) {
+                echo 'Search for &quot;'.wp_specialchars($s).'&quot; - ';
+            } elseif (!(is_404()) && (is_single()) || (is_page())) {
+                wp_title(''); echo ' - ';
+            } elseif (is_404()) {
+                echo 'Not Found - ';
+            } bloginfo('name'); ?>
+		</title>
+		<base href="<?php bloginfo('url'); ?>">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<!-- Set Google Site Verification code for Search Console -->
+		<meta name="google-site-verification" content="">
+		<!-- External CSS -->
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
+		<link href="http://gmpg.org/xfn/11" rel="profile" />
+		<link href="<?php bloginfo( 'pingback_url' ); ?>" rel="pingback" />
 </head>
 <body>
 
