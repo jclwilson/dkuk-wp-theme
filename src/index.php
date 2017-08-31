f3621e1a3934b039323e99351e5e76ac471a465d
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-08-31 19:20:22 +0100

    Adds critical Meyer Reset CSS to head, moves stylesheet link

--- src/index.php
+++ src/index.php
@@ -25,10 +25,15 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<!-- Set Google Site Verification code for Search Console -->
 		<meta name="google-site-verification" content="">
-		<!-- External CSS -->
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
 		<link href="http://gmpg.org/xfn/11" rel="profile" />
 		<link href="<?php bloginfo( 'pingback_url' ); ?>" rel="pingback" />
+		<!-- CSS -->
+		<style>
+			/* Inline CSS */
+			html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{border:0;font-size:100%;font:inherit;vertical-align:baseline;margin:0;padding:0}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:none}table{border-collapse:collapse;border-spacing:0}
+		</style>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
+		<!-- End of CSS -->
 		<!-- Scripts -->
 		<script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script>
 		<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
