a5253b61cc418cee3fd607f946048cbed4b89b77
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-08-31 17:53:31 +0100

    Adds link for site header / title

--- src/index.php
+++ src/index.php
@@ -32,15 +32,17 @@
 </head>
 <body>
 	<header>
-		<?php
-			$custom_logo_id = get_theme_mod( 'custom_logo' );
-			$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-			if ( has_custom_logo() ) {
-				echo '<img src="'. esc_url( $logo[0] ) .'">';
-			} else {
-				echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
-			}
-		?>
+		<a href="<?php bloginfo( 'pingback_url' ); ?>">
+			<?php
+				$custom_logo_id = get_theme_mod( 'custom_logo' );
+				$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+				if ( has_custom_logo() ) {
+					echo '<img src="'. esc_url( $logo[0] ) .'">';
+				} else {
+					echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+				}
+			?>
+		</a>
 		<nav>
 			<?php
 				wp_nav_menu(
