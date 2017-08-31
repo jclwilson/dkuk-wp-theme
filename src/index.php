935d1a02498295e2fc5a975a9a2f129eac3d935d
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-08-31 18:43:57 +0100

    Corrects stupid typo link in site header

--- src/index.php
+++ src/index.php
@@ -35,7 +35,7 @@
 </head>
 <body>
 	<header>
-		<a href="<?php bloginfo( 'pingback_url' ); ?>">
+		<a href="<?php bloginfo('url'); ?>">
 			<?php
 				$custom_logo_id = get_theme_mod( 'custom_logo' );
 				$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
