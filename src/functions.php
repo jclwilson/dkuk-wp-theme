f7020c1a5db856887865e3220f1bf392d60d6367
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-01 12:09:45 +0100

    Adds custom post types for exhibtion and podcast

--- src/functions.php
+++ src/functions.php
@@ -74,6 +74,115 @@ function posts_link_attributes_2() {
     return 'class="next-post__link"';
 }
 
+// Adds Custom Post Types
+// Made by @jclwilson
+// https://generatewp.com/post-type/
+
+function dkuk_custom_posts() {
+
+	$exhibition_labels = array(
+		'name'                  => _x( 'Exhibitions', 'Post Type General Name', 'dkuk' ),
+		'singular_name'         => _x( 'Exhibition', 'Post Type Singular Name', 'dkuk' ),
+		'menu_name'             => __( 'Exhibitions', 'dkuk' ),
+		'name_admin_bar'        => __( 'Exhibition', 'dkuk' ),
+		'archives'              => __( 'Exhibition Archives', 'dkuk' ),
+		'attributes'            => __( 'Exhibition Attributes', 'dkuk' ),
+		'parent_item_colon'     => __( 'Parent Exhibition:', 'dkuk' ),
+		'all_items'             => __( 'All Exhibitions', 'dkuk' ),
+		'add_new_item'          => __( 'Add New Exhibition', 'dkuk' ),
+		'add_new'               => __( 'Add New', 'dkuk' ),
+		'new_item'              => __( 'New Exhibition', 'dkuk' ),
+		'edit_item'             => __( 'Edit Exhibition', 'dkuk' ),
+		'update_item'           => __( 'Update Exhibition', 'dkuk' ),
+		'view_item'             => __( 'View Exhibition', 'dkuk' ),
+		'view_items'            => __( 'View Exhibitions', 'dkuk' ),
+		'search_items'          => __( 'Search Exhibition', 'dkuk' ),
+		'not_found'             => __( 'Not found', 'dkuk' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'dkuk' ),
+		'featured_image'        => __( 'Featured Image', 'dkuk' ),
+		'set_featured_image'    => __( 'Set featured image', 'dkuk' ),
+		'remove_featured_image' => __( 'Remove featured image', 'dkuk' ),
+		'use_featured_image'    => __( 'Use as featured image', 'dkuk' ),
+		'insert_into_item'      => __( 'Insert into exhibtion', 'dkuk' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Exhibition', 'dkuk' ),
+		'items_list'            => __( 'Exhibitions list', 'dkuk' ),
+		'items_list_navigation' => __( 'Exhibitons list navigation', 'dkuk' ),
+		'filter_items_list'     => __( 'Filter exhibitions list', 'dkuk' ),
+	);
+	$exhibition_args = array(
+		'label'                 => __( 'Exhibition', 'dkuk' ),
+		'description'           => __( 'Used for exhibitions put on by DKUK', 'dkuk' ),
+		'labels'                => $exhibition_labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-image',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'Exhibition', $exhibition_args );
+
+	$podcast_labels = array(
+		'name'                  => _x( 'Podcasts', 'Post Type General Name', 'dkuk' ),
+		'singular_name'         => _x( 'Podcast', 'Post Type Singular Name', 'dkuk' ),
+		'menu_name'             => __( 'Podcasts', 'dkuk' ),
+		'name_admin_bar'        => __( 'Podcast', 'dkuk' ),
+		'archives'              => __( 'Podcast Archive', 'dkuk' ),
+		'attributes'            => __( 'Podcast Attributes', 'dkuk' ),
+		'parent_item_colon'     => __( 'Parent Podcast:', 'dkuk' ),
+		'all_items'             => __( 'All Podcasts', 'dkuk' ),
+		'add_new_item'          => __( 'Add New Podcast', 'dkuk' ),
+		'add_new'               => __( 'Add New', 'dkuk' ),
+		'new_item'              => __( 'New Podcast', 'dkuk' ),
+		'edit_item'             => __( 'Edit Podcast', 'dkuk' ),
+		'update_item'           => __( 'Update Podcast', 'dkuk' ),
+		'view_item'             => __( 'View Podcast', 'dkuk' ),
+		'view_items'            => __( 'View Podcasts', 'dkuk' ),
+		'search_items'          => __( 'Search Podcasts', 'dkuk' ),
+		'not_found'             => __( 'Not found', 'dkuk' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'dkuk' ),
+		'featured_image'        => __( 'Featured Image', 'dkuk' ),
+		'set_featured_image'    => __( 'Set featured image', 'dkuk' ),
+		'remove_featured_image' => __( 'Remove featured image', 'dkuk' ),
+		'use_featured_image'    => __( 'Use as featured image', 'dkuk' ),
+		'insert_into_item'      => __( 'Insert into podcast', 'dkuk' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this podcast', 'dkuk' ),
+		'items_list'            => __( 'Podcasts list', 'dkuk' ),
+		'items_list_navigation' => __( 'Podcasts list navigation', 'dkuk' ),
+		'filter_items_list'     => __( 'Filter podcasts list', 'dkuk' ),
+	);
+	$podcast_args = array(
+		'label'                 => __( 'Podcast', 'dkuk' ),
+		'description'           => __( 'Used for podcasts made by DKUK', 'dkuk' ),
+		'labels'                => $podcast_labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 6,
+		'menu_icon'             => 'dashicons-microphone',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'Podcast', $podcast_args );
+
+}
+add_action( 'init', 'dkuk_custom_posts', 0 );
+
 // Hide Dashboard Widgets
 // by @jclwilson
 
