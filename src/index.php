2ab811c9a30765287ccebee6017b7dcaf589c2bd
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-04 15:25:03 +0100

    Adds BEM classes for index

--- src/index.php
+++ src/index.php
@@ -1,25 +1,25 @@
 	<?php get_header(); ?>
-	<main class="grid">
+	<main class="main grid">
 		<div class="grid__sizer"></div>
 		<div class="grid__gutter-sizer"></div>
 		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<article class="grid__item">
+				<article <?php post_class( 'grid__item' ); ?>>
 					<?php get_template_part( 'content', 'title' ); ?>
 					<?php if ( has_post_thumbnail() ) : ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+						<a class="thumbnail__link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 							<?php the_post_thumbnail(); ?>
 						</a>
 					<?php endif; ?>
-					<section>
+					<section class="post__excerpt">
 						<?php the_excerpt(); ?>
 					</section>
 				</article>
 			<?php endwhile; ?>
 			<!-- End of the main loop -->
 			<nav class="pagination">
-				<div><?php next_posts_link( 'Older posts' ); ?></div>
-				<div><?php previous_posts_link( 'Newer posts' ); ?></div>
+				<div class="pagination__older"><?php next_posts_link( 'Older posts' ); ?></div>
+				<div class="pagination__newer"><?php previous_posts_link( 'Newer posts' ); ?></div>
 			</nav>
 		<?php else : ?>
 			<article class="grid__item">
