e7e35f72ccfe782f07c76a5c074f9242e22825c8
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-04 15:15:16 +0100

    Adds archive-exhibition.php, lists titles only per existing site

---
+++ src/archive-exhibition.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<main class="grid">
+	<div class="grid__sizer"></div>
+	<div class="grid__gutter-sizer"></div>
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article class="grid__item">
+				<?php get_template_part( 'content', 'title' ); ?>
+			</article>
+		<?php endwhile; ?>
+		<!-- End of the main loop -->
+		<nav class="pagination">
+			<div><?php next_posts_link( 'Older posts' ); ?></div>
+			<div><?php previous_posts_link( 'Newer posts' ); ?></div>
+		</nav>
+	<?php else : ?>
+		<article class="grid__item">
+			<?php _e('Sorry, no posts matched your criteria.'); ?>
+		</article>
+	<?php endif; ?>
+</main>
+<?php get_footer(); ?>
