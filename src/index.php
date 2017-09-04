df20760900cf1a7763159a555b3eb7231c488456
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-04 15:02:39 +0100

    Corrects indentation of index.php

--- src/index.php
+++ src/index.php
@@ -3,13 +3,13 @@
 		<div class="grid__sizer"></div>
 		<div class="grid__gutter-sizer"></div>
 		<?php if ( have_posts() ) : ?>
-    		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 				<article class="grid__item">
 					<?php get_template_part( 'content', 'title' ); ?>
 					<?php if ( has_post_thumbnail() ) : ?>
-					    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					        <?php the_post_thumbnail(); ?>
-					    </a>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+							<?php the_post_thumbnail(); ?>
+						</a>
 					<?php endif; ?>
 					<section>
 						<?php the_excerpt(); ?>
@@ -47,7 +47,7 @@
 							</section>
 						<?php endif; ?>
 				</article>
-    		<?php endwhile; ?>
+			<?php endwhile; ?>
 			<!-- End of the main loop -->
 			<nav class="pagination">
 				<div><?php next_posts_link( 'Older posts' ); ?></div>
