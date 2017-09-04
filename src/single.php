f1dd0d3b29e798063e703840fc5055af3d7ba687
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-04 15:14:15 +0100

    Creates single.php

---
+++ src/single.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<main>
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article>
+				<?php get_template_part( 'content', 'title' ); ?>
+				<!-- Gallery content -->
+				<?php get_template_part( 'content', 'gallery' ); ?>
+				<section>
+					<?php the_content(); ?>
+				</section>
+			</article>
+		<?php endwhile; ?>
+		<!-- End of the main loop -->
+	<?php endif; ?>
+</main>
+<?php get_footer(); ?>
