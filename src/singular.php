ddf10001908ac9a8678406bbdcc98dd5157077a2
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-08 17:39:49 +0100

    Add podcast and salon sections to singular to save making multiple template files

--- src/singular.php
+++ src/singular.php
@@ -1,21 +1,27 @@
 <?php get_header(); ?>
 	<main class="main row">
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<article <?php post_class(); ?>>
-						<?php get_template_part( 'content', 'title' ); ?>
-						<!-- Either Gallery, or thumbnail if no gallery, or none if neither -->
-						<?php if ( get_field( "gallery" ) ) : ?>
-							<?php get_template_part( 'content', 'gallery' ); ?>
-						<?php elseif ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail(); ?>
-						<?php endif; ?>
-						<section class="post__content">
-							<?php the_content(); ?>
-						</section>
-					</article>
-				<?php endwhile; ?>
-				<!-- End of the main loop -->
-			<?php endif; ?>
-		</main>
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article <?php post_class('col-md-6 col-md-offset-3 singular'); ?>>
+					<?php get_template_part( 'content', 'title' ); ?>
+					<!-- Either Gallery, or thumbnail if no gallery, or none if neither -->
+					<?php if ( get_field( "gallery" ) ) : ?>
+						<?php get_template_part( 'content', 'gallery' ); ?>
+					<?php elseif ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail(); ?>
+					<?php endif; ?>
+					<?php if ( get_field( "podcast_link" ) ) : ?>
+						<?php get_template_part( 'content', 'podcast' ); ?>
+					<?php endif; ?>
+					<section class="post__content">
+						<?php the_content(); ?>
+					</section>
+					<?php if ( get_field( "salon_prices" ) ) : ?>
+						<?php get_template_part( 'content', 'salon' ); ?>
+					<?php endif; ?>
+				</article>
+			<?php endwhile; ?>
+			<!-- End of the main loop -->
+		<?php endif; ?>
+	</main>
 <?php get_footer(); ?>
