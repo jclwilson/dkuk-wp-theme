3af7ff6d74e61ed470e263202e2ab11268264e81
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2017-09-13 12:28:54 +0100

    Adds BEM classnames to content-salon.php

--- src/content-salon.php
+++ src/content-salon.php
@@ -1,16 +1,16 @@
 <?php if( have_rows('salon_prices') ): ?>
 	<section class="salon">
 		<table class="salon__prices">
-			<thead>
+			<thead class="salon__header">
 				<tr>
-					<th>Haircut</th>
-					<th>Price</th>
+					<th class="salon__heading">Haircut</th>
+					<th class="salon__heading">Price</th>
 				</tr>
 			</thead>
-			<tbody>
+			<tbody class="salon__main">
 				<?php while( have_rows('salon_prices') ): the_row(); ?>
-					<tr>
-						<td><?php the_sub_field('haircut'); ?></td><td><?php the_sub_field('price'); ?></td>
+					<tr class="salon__item">
+						<td class="salon__haircut"><?php the_sub_field('haircut'); ?></td><td class="salon__price"><?php the_sub_field('price'); ?></td>
 					</tr>
 				<?php endwhile; ?>
 			</tbody>
