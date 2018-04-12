5240af0883bfb949a965bfe34cb4bf97b52b95f8
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2018-04-12 17:02:17 +0100

    Puts scripts in index/singular conditionals to prevent errors

--- src/footer.php
+++ src/footer.php
@@ -27,59 +27,60 @@
 		</div>
 	</div>
 </footer>
-<script>
-	// check to see whether the document has loaded
-	document.addEventListener("DOMContentLoaded", function() {
-		// Checks to see whether all images have loaded in .main
-		imagesLoaded( document.querySelector('.main'), function( instance ) {
-			// init Masonry
-			var msnry = new Masonry( '.grid', {
-				// Masonry options...
-				itemSelector: '.grid__item',
-				columnWidth: '.grid__sizer',
-				gutter: '.grid__gutter-sizer',
-				percentPosition: true,
-			});
-
-			// init Infinite Scroll
-			var infScroll = new InfiniteScroll( '.scroll', {
-				// Infinite Scroll options...
-	  		  append: '.scroll__item',
-	  		  outlayer: msnry,
-	  		  path: '.prev-post__link',
-	  		  hideNav: '.pagination',
-			  // disable loading on scroll
-			  loadOnScroll: false,
-			  status: '.page-load-status',
-			});
-
-			// load next page & enable loading on scroll on button click
-			var viewMoreButton = document.querySelector('.view-more__button');
-			if(viewMoreButton !== undefined && viewMoreButton !== null) {
-				viewMoreButton.addEventListener( 'click', function() {
-					console.log("clicked");
-				  // load next page
-				  infScroll.loadNextPage();
-				  // enable loading on scroll
-				  infScroll.options.loadOnScroll = true;
-				  // hide page
-				  viewMoreButton.style.display = 'none';
-				});
-			}
-			// Ends Infinite Scroll Init
-
+<?php if ( is_singular() ) : ?>
+	<script>
+		// Only load below if it's on a singular page
+		document.addEventListener("DOMContentLoaded", function() {
 			// init Reframe.js
 			reframe('iframe');
+		});
+	</script>
+<?php else : ?>
+	<script>
+		// check to see whether the document has loaded
+		document.addEventListener("DOMContentLoaded", function() {
+			// Checks to see whether all images have loaded in .main
+			imagesLoaded( document.querySelector('.main'), function( instance ) {
+				// init Masonry
+				var msnry = new Masonry( '.grid', {
+					// Masonry options...
+					itemSelector: '.grid__item',
+					columnWidth: '.grid__sizer',
+					gutter: '.grid__gutter-sizer',
+					percentPosition: true,
+				});
 
-			// Init FLickity
-			var flkty = new Flickity( '.gallery__list', {
-				cellSelector: '.gallery__item',
-				wrapAround: true,
-				autoPlay: true,
+				// init Infinite Scroll
+				var infScroll = new InfiniteScroll( '.scroll', {
+					// Infinite Scroll options...
+				  append: '.scroll__item',
+				  outlayer: msnry,
+				  path: '.prev-post__link',
+				  hideNav: '.pagination',
+				  // disable loading on scroll
+				  loadOnScroll: false,
+				  status: '.page-load-status',
+				});
+
+				// load next page & enable loading on scroll on button click
+				var viewMoreButton = document.querySelector('.view-more__button');
+
+				if (viewMoreButton !== undefined && viewMoreButton !== null) {
+					viewMoreButton.addEventListener( 'click', function() {
+						console.log("clicked");
+					  // load next page
+					  infScroll.loadNextPage();
+					  // enable loading on scroll
+					  infScroll.options.loadOnScroll = true;
+					  // hide page
+					  viewMoreButton.style.display = 'none';
+					});
+				}
+				// Ends Infinite Scroll Init
 			});
 		});
-	});
-</script>
+	</script>
+<?php endif; ?>
 <?php wp_footer(); ?>
 <script>
 	if ('serviceWorker' in navigator) {
