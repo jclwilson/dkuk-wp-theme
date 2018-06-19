4e27d50d2913ffcba2d101ea2e7292b5c7939dcb
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2018-06-19 16:18:41 +0100

    Adds Google Tag Manager code

--- src/header.php
+++ src/header.php
@@ -52,8 +52,19 @@
 		It goes here to avoid us overwriting any of the scripts -->
 		<?php wp_head(); ?>
 		<!-- End of wp_head -->
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PBHMGVG');</script>
+		<!-- End Google Tag Manager -->
 </head>
 <body class="container-fluid">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBHMGVG"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<header class="header row center-xs large stripes">
 		<div class="header__container col-xs">
 			<?php if ( has_custom_logo() ) : ?>
