2e77b85c87da0eb32e425d11c87a81526fe66980
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2018-07-01 14:22:29 +0100

    Adds tagline to site header

--- src/header.php
+++ src/header.php
@@ -78,7 +78,7 @@
 					<h1 class="header__title">
 						<a class="header__link" href="<?php bloginfo('url'); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>">
 							<img class="header__logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg" alt="<?php bloginfo( 'name' ); ?>" />
-							<span class="header__logo-extra">Salon</span>
+							<span class="header__logo-extra"><?php  bloginfo('description'); ?></span>
 						</a>
 					</h1>
 				</div>
