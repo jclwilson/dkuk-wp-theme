cccc615de78b3dc6643855f068537447ab3e6a56
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2018-09-19 10:39:19 +0100

    Updates sw.js to prevent running on GET, admin, preview

--- src/sw.js
+++ src/sw.js
@@ -1,38 +1,55 @@
-    var CACHE_VERSION = 'DKUK-1';
-	var CACHE_FILES = [
-        '/wp-content/themes/dkuk-wp-theme/style.css',
-        '/wp-content/themes/dkuk-wp-theme/assets/fonts/WorkSans-Regular.ttf',
-        '/wp-content/themes/dkuk-wp-theme/assets/fonts/WorkSans-Bold.ttf',
-	    '/wp-content/themes/dkuk-wp-theme/assets/js/main.min.js',
-        '/wp-content/themes/dkuk-wp-theme/assets/img/logo.svg'
-	];
+var CACHE_VERSION = 'DKUK-5';
+var CACHE_FILES = [
+	'/wp-content/themes/dkuk-wp-theme/style.css',
+	'/wp-content/themes/dkuk-wp-theme/assets/fonts/WorkSans-Regular.ttf',
+	'/wp-content/themes/dkuk-wp-theme/assets/fonts/WorkSans-Bold.ttf',
+	'/wp-content/themes/dkuk-wp-theme/assets/js/main.min.js',
+	'/wp-content/themes/dkuk-wp-theme/assets/img/logo.svg'
+];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_VERSION).then(function(cache) {
-            return cache.addAll(CACHE_FILES);
-        })
-    );
+	event.waitUntil(
+		caches.open(CACHE_VERSION).then(function(cache) {
+			return cache.addAll(CACHE_FILES);
+		})
+	);
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+	// Stop the service worker chrome devtools bug
+	// https://bugs.chromium.org/p/chromium/issues/detail?id=823392
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+		return;
+	}
+
+	//This service worker won't touch non-get requests
+	if (event.request.method != 'GET') {
+		return;
+	}
+	
+	//This service worker won't touch the admin area and preview pages
+	if (event.request.url.match(/wp-admin/) || event.request.url.match(/preview=true/)) {
+		return;
+	}
+
+	// Then check cache, and then network if no cache.
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(function(keys) {
-            return Promise.all(
-                keys.map(function(key, i) {
-                    if (key !== CACHE_VERSION) {
-                        return caches.delete(keys[i]);
-                    }
-                })
-            )
-        })
-    );
+	event.waitUntil(
+		caches.keys().then(function(keys) {
+			return Promise.all(
+				keys.map(function(key, i) {
+					if (key !== CACHE_VERSION) {
+						return caches.delete(keys[i]);
+					}
+				})
+			)
+		})
+	);
 });
